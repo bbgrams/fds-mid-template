01cf992d5c261329853103272065a13d0460f938
Shim Soyoung <sovv12@gmail.com>
2018-11-07 17:01:44 +0900

    메인에 상품 목록(이미지, 상품명) 출력

--- src/index.js
+++ src/index.js
@@ -18,9 +18,14 @@ api.interceptors.request.use(function (config) {
 
 const templates = {
   loginForm : document.querySelector('#login-form').content,
+  productsForm : document.querySelector('#products-form').content,
+  productsItems: document.querySelector('#product-items').content,
+
 }
 
 const rootEl = document.querySelector('.root')
+const pageTitleEl = document.querySelector('.page-title') // 페이지 별 타이틀
+let pageTitle= '빕다방'
 
 // 페이지 그리는 함수 작성 순서
 // 1. 템플릿 복사
@@ -29,9 +34,40 @@ const rootEl = document.querySelector('.root')
 // 4. 내용 채우기
 // 5. 이벤트 리스너 등록하기
 // 6. 템플릿을 문서에 삽입
+// 2. 메인화면
+async function drawMain() {
+  pageTitle = '빕다방의 메뉴' // 메인화면일 때의 타이틀 텍스트
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.productsForm, true)
+  // 2. 요소 선택
+  const productListEl = frag.querySelector('.products-list')
+  // 3. 필요한 데이터 불러오기
+  const { data: productsList } = await api.get('/products?_embed=options')
+  // 4. 내용 채우기
+  // 상품 목록 나열
+  for (const productsItems of productsList){
+    // 1. 템플릿 복사
+    const frag = document.importNode(templates.productsItems, true)
+    // 2. 요소 선택
+    const thumbnailEl = frag.querySelector('.thumbnail')
+    const nameEl = frag.querySelector('.name')
+    // 3. 필요한 데이터 불러오기
+    // 4. 내용 채우기
+    thumbnailEl.setAttribute('src', productsItems.mainImgUrl);
+    nameEl.textContent = productsItems.title;
+    // 5. 이벤트 리스너 등록하기
+    // 6. 템플릿을 문서에 삽입
+    productListEl.appendChild(frag)
 
-// 로그인 화면
-function drawLoginForm(){
+  }
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
+  rootEl.appendChild(frag)
+}
+
+
+// 1. 로그인 화면
+async function drawLoginForm(){
   // 1. 템플릿 복사
   const frag = document.importNode(templates.loginForm, true)
   // 2. 요소 선택
@@ -51,12 +87,16 @@ function drawLoginForm(){
     })
 
     localStorage.setItem('token', res.data.token)
-    // drawMain()
+    drawMain()
     alert(`${username}님 로그인 되었습니다.`)
   })
   // 6. 템플릿을 문서에 삽입
+  drawMain()
   rootEl.textContent = '';
   rootEl.appendChild(frag)
 }
 
-drawLoginForm()
+drawMain()
+
+pageTitleEl.textContent = pageTitle; // 페이지별 타이틀
+
